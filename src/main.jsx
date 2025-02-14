4bae599aeb422844bd8fb89ca751b1c5c06c0bad
Kyaw Zin Thet <kyawzinthetpersonal@gmail.com>
2025-02-14 23:11:36 +0630

    react component

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,32 @@
 import { createRoot } from "react-dom/client";
+import "./App.css";
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 
-root.render(<h1>Hello React</h1>);
+function Header() {
+  return (
+    <div>
+      <img src="/react-logo.png" alt="react-logo" width="40px" />
+    </div>
+  );
+}
+
+function Footer(){
+    return (
+        <footer>&copy; abo development. All rights reversed.</footer>
+    );
+}
+
+root.render(
+  <div>
+    <Header />
+  <main>
+    <h1>Reason I'm excited to learn React</h1>
+    <ol>
+        <li>React is popular</li>
+        <li>React is cool</li>
+    </ol>
+  </main>
+  <Footer/>
+  </div>
+);
