84e9854d9d27756f446de4402a1a9142f4d31020
Kyaw Zin Thet <kyawzinthetpersonal@gmail.com>
2025-02-16 00:15:03 +0630

    initial project setup

---
+++ src/components/Main.jsx
@@ -0,0 +1,7 @@
+export default function Main() {
+  return (
+    <main>
+      <h1>Main content is here</h1>
+    </main>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,8 @@
 import { createRoot } from "react-dom/client";
-import "./App.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Main from "./components/MainContent";
+import App from "./App";
 
 const root = createRoot(document.getElementById("root"));
 
 root.render(
-  <>
-    <Header />
-    <Main />
-    <Footer />
-  </>
+  <App />
 );
