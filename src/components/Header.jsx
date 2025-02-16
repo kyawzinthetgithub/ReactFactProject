c37aaae7d0d11cf1fad10cfa4572d412889ed6ae
Kyaw Zin Thet <kyawzinthetpersonal@gmail.com>
2025-02-16 23:38:00 +0630

    props in react

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header () {
+    return (
+        <>
+            <header>
+                <div>
+                    <img src="/images/globe.png" alt="header logo" width="20px" />
+                    <p>my travel journal.</p>
+                </div>
+            </header>
+        </>
+    );
+}
